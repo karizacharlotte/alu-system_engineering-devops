0b404f48168e124db51ab000f3be8a0df14f9390
karizacharlotte <c.kariza@alustudent.com>
2024-07-11 18:29:02 +0000

    SSH

---
+++ 100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+
+ PasswordAuthentication using puppet
+# SSH configuration file
+file { '/etc/ssh/ssh_config':
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+  content => "
+    Host *
+      IdentityFile ~/.ssh/school
+      PasswordAuthentication no
+  ",
+}
